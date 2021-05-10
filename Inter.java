118db4be3080840afc118357a2685a23d8d67feb
Mr.Stark <shubhamnagar0999@gmail.com>
2021-05-10 10:48:50 -0700

    first commit

---
+++ Inter.java
@@ -0,0 +1,38 @@
+package Action;
+interface I{
+    public void speak();
+}
+class J implements I{
+    public void speak(){
+        System.out.println("I can speak");
+    }
+}
+
+class K implements I{
+  public void speak(){
+      System.out.println("I can also speak");
+  }
+}
+public class Inter{
+    public static void main(String args[]){
+        J O = new J();
+        O.speak();
+
+        K o1 = new K();
+        o1.speak();
+
+        //With lembda expression
+        I o = () -> {
+          System.out.println("i am your papa");
+          };
+      o.speak();
+        }
+        //With anaoymose class
+        I O1 = new I() {
+            @Override
+            public void speak() {
+                System.out.println("i can dance.");
+            }
+        };
+
+    }
